49f0ff5705f1a28ad6b49b14bd059e5c61dd6542
farhanfaahiz <105285509+farhanfaahiz@users.noreply.github.com>
2023-02-18 14:05:10 +0100

    Add files via upload

---
+++ beginner 47 lambda with filter, map, reduce in python.py
@@ -0,0 +1,38 @@
+#def double(x):
+#    return x * 2
+#def add(x, y):
+#    return x + y
+#def product(x, y, z):
+#    return x * y * z 
+
+# In lambda form
+from functools import reduce
+
+
+double = lambda x : x * 2
+add = lambda x , y : x + y  
+product = lambda x , y , z : x * y * z
+
+print(double(10))
+print(add(10 ,20))
+print(product(10, 20, 30))
+
+# filter, reduce and map
+my_list = [2, 5, 8, 10, 9, 3]
+
+a = map(lambda x : x * 2, my_list)
+print(list(a))
+
+my_list2 = [3, 7, 5, 9, 1]
+b = map(lambda x , y : x + y, my_list, my_list2)
+print(list(b))
+
+# filter
+c = filter(lambda x : x%2 == 0, my_list)
+print(list(c))
+d = filter(lambda x : True if x >= 5 else False, my_list)
+print(list(d)) 
+
+#reduce
+e = reduce(lambda x , y: x + y, my_list)
+print(e)
